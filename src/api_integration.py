577622a8037a70a9e73fd7724310b92c0fc53857
SaurabhIndi <saurabhyadavaligarh@gmail.com>
2024-11-13 19:22:37 +0530

    links updated

--- src/api_integration.py
+++ src/api_integration.py
@@ -15,17 +15,33 @@ def fetch_data(endpoint, page=1, page_size=100):
         print(f"Error fetching data from {endpoint}: {response.status_code}")
         return {}
 
+# def get_all_viewed_posts():
+#     return fetch_data("/posts/view")
+
+# def get_all_liked_posts():
+#     return fetch_data("/posts/like")
+
+# def get_all_user_ratings():
+#     return fetch_data("/posts/rating")
+
+# def get_all_posts():
+#     return fetch_data("/posts/summary/get")
+
+# def get_all_users():
+#     return fetch_data("/users/get_all")
+
+
 def get_all_viewed_posts():
-    return fetch_data("/posts/view")
+    return fetch_data("https://api.socialverseapp.com/posts/view?page=1&page_size=1000&resonance_algorithm=resonance_algorithm_cjsvervb7dbhss8bdrj89s44jfjdbsjd0xnjkbvuire8zcjwerui3njfbvsujc5if")
 
 def get_all_liked_posts():
-    return fetch_data("/posts/like")
+    return fetch_data("https://api.socialverseapp.com/posts/like?page=1&page_size=5&resonance_algorithm=resonance_algorithm_cjsvervb7dbhss8bdrj89s44jfjdbsjd0xnjkbvuire8zcjwerui3njfbvsujc5if")
 
 def get_all_user_ratings():
-    return fetch_data("/posts/rating")
+    return fetch_data("https://api.socialverseapp.com/posts/rating?page=1&page_size=5&resonance_algorithm=resonance_algorithm_cjsvervb7dbhss8bdrj89s44jfjdbsjd0xnjkbvuire8zcjwerui3njfbvsujc5if")
 
 def get_all_posts():
-    return fetch_data("/posts/summary/get")
+    return fetch_data("https://api.socialverseapp.com/posts/summary/get?page=1&page_size=1000")
 
 def get_all_users():
-    return fetch_data("/users/get_all")
+    return fetch_data("https://api.socialverseapp.com/users/get_all?page=1&page_size=1000")
